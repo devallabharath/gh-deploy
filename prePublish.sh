899aed52b2567e99f24553bff6878fa332ee70de
Bharath <devallabharath619@gmail.com>
2024-01-07 17:48:47 -0500

    build: ⚡️ more script efficiency

--- prePublish.sh
+++ prePublish.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/bash
-rm -rf release/
+#!/usr/bin/env bash
+npm run package
+rm -rf release
 mkdir release
+mkdir release/dist
 cp -r dist/ release/dist/
 cp CHANGELOG.md README.md package.json release/
